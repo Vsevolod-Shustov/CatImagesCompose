8e86d2a36289cefdcfb1f0add09ffd4ea7ded90a
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2025-12-10 14:19:28 +0300

    single image composable

---
+++ app/src/main/java/com/example/catimagescompose/ui/SingleImage.kt
@@ -0,0 +1,36 @@
+package com.example.catimagescompose.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.catimagescompose.data.ImageDataModel
+
+@Composable
+fun SingleImage(photo: ImageDataModel) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Red.copy(alpha = 0.1f))
+            .padding(20.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        Column {
+            ImageCard(photo)
+            Text(
+                "Item: ${photo.id}",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
