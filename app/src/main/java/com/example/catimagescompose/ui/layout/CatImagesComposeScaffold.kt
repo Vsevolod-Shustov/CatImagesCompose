dbc485c332cec39fce8929fc7e94e7e3badad634
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2025-12-15 14:46:53 +0300

    display navbars on narrow screens, rail on wide screens

--- app/src/main/java/com/example/catimagescompose/ui/layout/CatImagesComposeScaffold.kt
+++ app/src/main/java/com/example/catimagescompose/ui/layout/CatImagesComposeScaffold.kt
@@ -3,18 +3,24 @@ package com.example.catimagescompose.ui.layout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
@@ -26,6 +32,7 @@ import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
+import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOWER_BOUND
 import com.example.catimagescompose.ui.ImageGrid
 import com.example.catimagescompose.ui.ListDetailSceneStrategy
 import com.example.catimagescompose.ui.SingleImage
@@ -46,13 +53,18 @@ fun CatImagesComposeScaffold(modifier: Modifier = Modifier, drawerState: DrawerS
 
     val backStack = remember { mutableStateListOf<NavKey>(Grid) }
     val listDetailStrategy = rememberListDetailSceneStrategy<NavKey>()
+    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
 
     Scaffold(
         topBar = {
-            CatImagesComposeTopAppBar(drawerState, scope)
+            if (!windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_DP_MEDIUM_LOWER_BOUND)) {
+                CatImagesComposeTopAppBar(drawerState, scope)
+            }
         },
         bottomBar = {
-            CatImagesComposeBottomAppBar()
+            if (!windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_DP_MEDIUM_LOWER_BOUND)) {
+                CatImagesComposeBottomAppBar()
+            }
         },
         floatingActionButton = {
             FloatingActionButton(onClick = { }) {
@@ -60,32 +72,50 @@ fun CatImagesComposeScaffold(modifier: Modifier = Modifier, drawerState: DrawerS
             }
         }
     ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            NavDisplay(
-                backStack = backStack,
-                onBack = { backStack.removeLastOrNull() },
-                sceneStrategy = listDetailStrategy,
-                entryProvider = entryProvider {
-                    entry<Grid>(
-                        metadata = ListDetailSceneStrategy.listPane()
-                    ) {
-                        Column {
-                            ImageGrid(backStack)
+        Row {
+            if (windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_DP_MEDIUM_LOWER_BOUND)) {
+                NavigationRail {
+                    NavigationRailItem(
+                        selected = false,
+                        onClick = { /* Navigate to Item 1 */ },
+                        label = { Text("Item 1") },
+                        icon = { Icon(Icons.Default.Home, contentDescription = "Item 1") }
+                    )
+                    NavigationRailItem(
+                        selected = false,
+                        onClick = { /* Navigate to Item 2 */ },
+                        label = { Text("Item 2") },
+                        icon = { Icon(Icons.Default.Person, contentDescription = "Item 2") }
+                    )
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                NavDisplay(
+                    backStack = backStack,
+                    onBack = { backStack.removeLastOrNull() },
+                    sceneStrategy = listDetailStrategy,
+                    entryProvider = entryProvider {
+                        entry<Grid>(
+                            metadata = ListDetailSceneStrategy.listPane()
+                        ) {
+                            Column {
+                                ImageGrid(backStack)
+                            }
+                        }
+                        entry<Single>(
+                            metadata = ListDetailSceneStrategy.detailPane()
+                        ) { image ->
+                            SingleImage(image.id)
                         }
                     }
-                    entry<Single>(
-                        metadata = ListDetailSceneStrategy.detailPane()
-                    ) { image ->
-                        SingleImage(image.id)
-                    }
-                }
-            )
+                )
+            }
         }
     }
 }
